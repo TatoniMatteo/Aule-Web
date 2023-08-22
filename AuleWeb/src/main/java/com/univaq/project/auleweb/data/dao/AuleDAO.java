e570b411d8d08cad8142769a3c99b7842ee2056c
Davide-Di-Rocco <davide.dirocco@student.univaq.it>
2023-08-22 11:57:26 +0200

    update

--- AuleWeb/src/main/java/com/univaq/project/auleweb/data/dao/AuleDAO.java
+++ AuleWeb/src/main/java/com/univaq/project/auleweb/data/dao/AuleDAO.java
@@ -1,5 +1,6 @@
 package com.univaq.project.auleweb.data.dao;
 
+import com.univaq.project.auleweb.data.model.Attrezzatura;
 import com.univaq.project.auleweb.data.model.Aula;
 import com.univaq.project.framework.data.DataException;
 import java.util.List;
@@ -17,4 +18,14 @@ public interface AuleDAO {
     List<Aula> getAuleByName(String filter) throws DataException;
 
     int getAuleNumber() throws DataException;
+
+    int insertAula(Aula aula) throws DataException;
+
+    void updateAula(Aula aula) throws DataException;
+
+    void updateAttrezzatura(int aulaId, Attrezzatura attrezzatura) throws DataException;
+
+    void assignGruppo(int aulaId, List<Integer> gruppiId) throws DataException;
+
+    void removeAssignGruppo(int aulaId, List<Integer> gruppiId) throws DataException;
 }
