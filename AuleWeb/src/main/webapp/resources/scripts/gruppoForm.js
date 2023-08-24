3e06d84ae3b011dcec50960a7d1901ef403fcb96
Matteo Tatoni <83955605+TatoniMatteo@users.noreply.github.com>
2023-08-24 17:07:15 +0200

    update

--- AuleWeb/src/main/webapp/resources/scripts/gruppoForm.js
+++ AuleWeb/src/main/webapp/resources/scripts/gruppoForm.js
@@ -1,21 +1,19 @@
 const searchInput = document.getElementById("searchInput");
 const optionsList = document.getElementById("optionsList");
-const optionItems = optionsList.querySelectorAll(".categoria");
+const categorie = optionsList.querySelectorAll(".categoria");
+const optionItems = optionsList.querySelectorAll("li");
 const selectedKeyInput = document.getElementById("selectedKey");
 
-
 searchInput.addEventListener("blur", function () {
-    // Aggiorna le opzioni solo se l'input perde il focus
+// Aggiorna le opzioni solo se l'input perde il focus
     setTimeout(function () {
         optionsList.style.display = "none";
     }, 200); // Aggiungi un ritardo per consentire la selezione dell'opzione
 });
-
 searchInput.addEventListener("input", function () {
     const searchTerm = searchInput.value.toLowerCase();
     let foundMatch = false;
-
-    optionItems.forEach(function (item) {
+    categorie.forEach(function (item) {
         const optionText = item.dataset.text.toLowerCase();
         if (optionText.includes(searchTerm)) {
             item.style.display = "block";
@@ -24,7 +22,6 @@ searchInput.addEventListener("input", function () {
             item.style.display = "none";
         }
     });
-
     // Mostra o nascondi l'opzione speciale "Nessuna corrispondenza"
     const noMatchOption = document.getElementById("no-match");
     if (foundMatch) {
@@ -32,26 +29,22 @@ searchInput.addEventListener("input", function () {
     } else {
         noMatchOption.style.display = "flex";
         const userInputElement = noMatchOption.querySelector(".user-input");
-        userInputElement.textContent = capitalize(searchTerm);
-        noMatchOption.setAttribute("data-text", capitalize(searchTerm));
+        const value = capitalize(searchTerm);
+        userInputElement.textContent = value;
+        noMatchOption.setAttribute("data-text", value);
     }
 
     optionsList.style.display = "block";
 });
-
 optionItems.forEach(function (item) {
     item.addEventListener("click", function () {
         const selectedText = item.dataset.text;
         const selectedKey = item.dataset.key;
-
         searchInput.value = selectedText;
         selectedKeyInput.value = selectedKey;
-
         optionsList.style.display = "none";
     });
 });
-
-
 function capitalize(str) {
     const words = str.split(" ");
     const capitalizedWords = words.map(word => {
